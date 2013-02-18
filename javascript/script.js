4a6da257239fec9545b26ca60f7b21492466a9cf
MCh <developer.mch@gmail.com>
2013-02-18 21:14:14 +0100

    druhý commit

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 	$(".first_menu > li").hover(function(o){
 		$(this).children("this > ul").css("border", "10px");
 	}, function(o){
-		//
+		// tu zatiaľ nič nieje
 	});
 
 });
